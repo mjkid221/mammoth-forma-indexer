7b5cf8097004681fc492d56dd68460c4b2cdea09
mj221 <mj@labrys.io>
2024-12-24 15:02:13 +1000

    test: testing cors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@
  */
 import "./src/env.js";
 
+function getBaseUrl() {
+  if (typeof window !== "undefined") return window.location.origin;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 /** @type {import("next").NextConfig} */
 const config = {
   headers: async () => {
@@ -15,6 +21,17 @@ const config = {
             key: "Cache-Control",
             value: "public, max-age=300, stale-while-revalidate=300",
           },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: getBaseUrl() },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
       },
       {
@@ -23,6 +40,17 @@ const config = {
           {
             key: "Cache-Control",
             value: "public, max-age=300, stale-while-revalidate=300",
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: getBaseUrl() },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
